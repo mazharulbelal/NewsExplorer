fe188c64a6a4e524a3f5780c54bb5cc7585dfda5
Mazharul <mazharul.belal@gmail.com>
2026-01-12 20:31:55 +0600

    Initialize iOS project structure

---
+++ NewsExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsExplorer
+//
+//  Created by Md Mazharul Islam on 12/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NewsExplorerTests/NewsExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsExplorerTests.swift
+//  NewsExplorerTests
+//
+//  Created by Md Mazharul Islam on 12/1/26.
+//
+
+import Testing
+@testable import NewsExplorer
+
+struct NewsExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
